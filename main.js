03c8b9b1ad2c50e08e6d51ba84483afe81e980e1
DylanWCCI <dylanvaca@wecancodeit.org>
2023-06-04 17:51:46 -0400

    Added search functionality and made edits to css.

--- main.js
+++ main.js
@@ -1,56 +1,90 @@
-//Three States of a Promise
-//Pending
-//Fulfilled
-//Rejected
+let searchInput = document.getElementById('search');
 
-//Two methods
-//.then
-//.catch
+document.getElementById('get-character').addEventListener('click', function() {
+fetchRandomCharacter();
+});
 
-//Asynchronous
+document.getElementById('get-all-characters').addEventListener('click', function() {
+fetchAllCharacters();
+});
 
-//GET
-fetch('https://swapi.dev/api/people/1/')
-.then(response => response.json())
-.then(data => console.log('Character: ', data.name))
-.catch(error => console.log("Error: ", error));
+searchInput.addEventListener('keyup', function(e){
+    let searchString = e.target.value;
+    if (searchString && searchString.length > 2) {
+        fetchSearchedCharacters(searchString);
+    }
+});
 
-//POST
-let characterData = {
-    name: 'Spock',
-    height: '100'
-};
+function fetchRandomCharacter() {
+    let randomID = Math.floor(Math.random() * 9) +1;
+    
+    fetch(`https://swapi.dev/api/people/${randomID}/`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('character').innerHTML =
+        `<h2>${data.name}</h2>
+        <p>Height: ${data.height}cm</p>
+        <p>Mass: ${data.mass}kg</p>
+        <p>Hair color: ${data.hair_color}</p>
+        <p>Skin Color: ${data.skin_color}</p>
+        <p>Eye Color: ${data.eye_color}</p>
+        <p>Birth Year: ${data.birth_year}</p>
+        <p>Gender: ${data.gender}</p>`;
+    })
+    .catch(error => console.log('Error: ', error));
+}
 
-// let requestOptions = {
-//     method: 'POST',
-//     headers: {'Content-Type': 'application/json'},
-//     body: JSON.stringify(characterData)
-// };
+function fetchAllCharacters() {
+    fetch('https://swapi.dev/api/people/')
+    .then(response => response.json())
+    .then(data => {
+        let characters = data.results;
+        let charactersList = document.getElementById('characters');
+        charactersList.innerHTML = '';
+        characters.forEach(character => {
+            let li = document.createElement('li');
+            li.textContent = character.name;
+            li.addEventListener('click', function() {
+                document.getElementById('character').innerHTML = `<h2>${character.name}</h2>
+                <p>Height: ${character.height}cm</p>
+                <p>Mass: ${character.mass}kg</p>
+                <p>Hair color: ${character.hair_color}</p>
+                <p>Skin Color: ${character.skin_color}</p>
+                <p>Eye Color: ${character.eye_color}</p>
+                <p>Birth Year: ${character.birth_year}</p>
+                <p>Gender: ${character.gender}</p>`;
+            })
+            charactersList.appendChild(li);
+        });
+    })
+    .catch(error => console.log('Error: ', error));
+}
 
-//PUT
-let updatedCharacterData = {
-    name: 'Spongebob',
-    height: '200'
-};
-
-// let requestOptions = {
-//     method: 'PUT',
-//     headers: {'Content-Type': 'application/json'},
-//     body: JSON.stringify(updatedCharacterData)
-// };
-
-// fetch('https://swapi.dev/api/people/1/', requestOptions)
-// .then(response => response.json())
-// .then(data => console.log('Put response: ', data))
-// .catch(error => console.log('Error: ', error));
-
-//DELETE
-
-let requestOptions = {
-    method: 'DELETE'
-};
-
-fetch('https://swapi.dev/api/people/1/', requestOptions)
-.then(response => response.status)
-.then(status => console.log('Delete response: ', status))
-.catch(error => console.log('Error: ', error));
+function fetchSearchedCharacters(searchString) {
+    fetch('https://swapi.dev/api/people/')
+        .then(response => response.json())
+        .then(data => {
+            let characters = data.results;
+            let filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(searchString.toLowerCase()));
+            let charactersList = document.getElementById('characters');
+            charactersList.innerHTML = ""; // clear the list before appending new characters
+            filteredCharacters.forEach(character => {
+                let li = document.createElement('li');
+                li.textContent = character.name;
+                li.addEventListener('click', function() {
+                    document.getElementById('character').innerHTML = `
+                        <h2>${character.name}</h2>
+                        <p>Height: ${character.height}cm</p>
+                        <p>Mass: ${character.mass}kg</p>
+                        <p>Hair Color: ${character.hair_color}</p>
+                        <p>Skin Color: ${character.skin_color}</p>
+                        <p>Eye Color: ${character.eye_color}</p>
+                        <p>Birth Year: ${character.birth_year}</p>
+                        <p>Gender: ${character.gender}</p>
+                    `;
+                });
+                charactersList.appendChild(li);
+            });
+        })
+        .catch(error => console.log('Error:', error));
+}
